2b634217008e801a42ef37393891c8ac1f2806b5
AlexPerez7 <alexmatiasp01@gmail.com>
2025-04-15 18:38:18 -0400

    Customización barra

--- CursorKeep.py
+++ CursorKeep.py
@@ -22,6 +22,21 @@ class MouseMoverApp:
         # Establecer ícono
         self.set_icon()
 
+        # Estilo personalizado para la barra de progreso
+        style = ttk.Style()
+        style.theme_use("default")
+        style.configure(
+            "custom.Horizontal.TProgressbar",
+            thickness=12,
+            troughcolor="#e0e0e0",
+            background="#2196f3",  # Azul moderno
+        )
+
+        # Estilo personalizado para la barra de progreso
+        style = ttk.Style()
+        style.theme_use("default")
+        style.configure("custom.Horizontal.TProgressbar", thickness=15)
+
         # Estado e intervalo
         self.running = False
         self.intervalo_var = tk.IntVar(value=30)
@@ -32,16 +47,24 @@ class MouseMoverApp:
 
         # Botón de inicio/detención
         self.btn_toggle = tk.Button(
-            frame_controles, text="Iniciar", font=self.fuente, width=20, command=self.toggle
+            frame_controles,
+            text="Iniciar",
+            font=self.fuente,
+            width=20,
+            command=self.toggle,
         )
         self.btn_toggle.pack(pady=(0, 10))
 
         # Estado del programa
-        self.status_label = tk.Label(frame_controles, text="Estado: Inactivo", font=self.fuente)
+        self.status_label = tk.Label(
+            frame_controles, text="Estado: Inactivo", font=self.fuente
+        )
         self.status_label.pack(pady=5)
 
         # Frame para seleccionar el intervalo
-        frame_intervalo = tk.LabelFrame(frame_controles, text="Intervalo de movimiento", font=self.fuente)
+        frame_intervalo = tk.LabelFrame(
+            frame_controles, text="Intervalo de movimiento", font=self.fuente
+        )
         frame_intervalo.pack(pady=5, fill="x", expand=True)
 
         intervalo_menu = ttk.Combobox(
@@ -56,8 +79,13 @@ class MouseMoverApp:
 
         # Barra de progreso
         self.progress_bar = ttk.Progressbar(
-            frame_controles, orient="horizontal", length=250, mode="determinate"
+            frame_controles,
+            orient="horizontal",
+            length=250,
+            mode="determinate",
+            style="custom.Horizontal.TProgressbar",
         )
+
         self.progress_bar.pack(pady=15)
         self.progress_bar["maximum"] = self.intervalo_var.get()
 
